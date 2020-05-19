574c5a98b710cdece8234360b79dbbfe9f592b51
Brian Schaefer <physinet@gmail.com>
2020-05-19 13:54:15 -0400

    Insert initial notebook
    Calculations look promising for each valley, didn't get macroscopic quantity calculations working yet

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
 setup(name='blg_strain',
-      version='0.1',
-      description='Band structure calculations for strained bilayer graphene',
-      url='http://github.com/physinet/blg_strain',
-      author='Brian Schaefer',
-      author_email='physinet@gmail.com',
-      license='MIT',
-      packages=['blg_strain'],
-      zip_safe=False)
+    version='0.1',
+    description='Band structure calculations for strained bilayer graphene',
+    url='http://github.com/physinet/blg_strain',
+    author='Brian Schaefer',
+    author_email='physinet@gmail.com',
+    license='MIT',
+    packages=['blg_strain'],
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scipy'
+    ],
+    zip_safe=False)
