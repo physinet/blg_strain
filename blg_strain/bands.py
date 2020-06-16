Fix big - xi not used for Berry calculation
When writing the class used to contain results of calculations, neglected to pass xi into calculation of Berry curvature.

Also uncovered an error in one version of the 2x2 Hamiltonians.

blg_strain/bands.py
@@ -32,6 +32,7 @@ def get_bands(kxlims=[-0.35e9, .35e9], kylims=[-0.35e9, .35e9], Nkx=200,
 
     Kx, Ky = np.meshgrid(kx, ky, indexing='ij')
 
+
     E, Psi = _get_bands(Kx, Ky, xi=xi, Delta=Delta, delta=delta,
                         theta=theta, twobytwo=twobytwo, eigh=eigh)
 
@@ -53,7 +54,6 @@ def _get_bands(Kx, Ky, xi=1, eigh=True, **params):
     - E: N(=4) x Nkx x Nky array of energy eigenvalues
     - Psi: N(=4) x N(=4) x Nkx x Nky array of eigenvectors
     '''
-
     H = Hfunc(Kx, Ky, xi=xi, **params)
 
     H = H.transpose(2,3,0,1) # put the 4x4 in the last 2 dims for eigh
@@ -74,12 +74,14 @@ def _get_bands(Kx, Ky, xi=1, eigh=True, **params):
     # now E[:, 0, 0] is a length-4 array of eigenvalues
     # and Psi[:, :, 0, 0] is a 4x4 array of eigenvectors (in the columns)
 
-    if not eigh:
+    if eigh:
+        Psi = fix_first_component_sign(Psi)
+
+    else:
         E, Psi = sort_eigen(E, Psi)
         E = E.real
 
-    if eigh:
-        Psi = force_first_component_positive(Psi)
+
 
 
     # Finally, transpose first 2 dimensions to put the eigenvectors in the rows
@@ -112,7 +114,7 @@ def sort_eigen(eigs, vecs):
     return eigs[tuple(indE)], vecs[tuple(indV)]
 
 
-def force_first_component_positive(Psi):
+def fix_first_component_sign(Psi):
     '''
     Addresses sign ambiguity in eigenvectors by focing first component positive
     '''

blg_strain/berry.py
@@ -41,7 +41,7 @@ def berry_mu(Kx, Ky, E, Psi, xi=1, einsum=True):
                 # i and l index the components of the eigenvectors for bands n and m
                 # the H' matrix is indexed with il to contract these indices
                 # j and k index over the kx, ky points and are the dimensions left
-                if hdkx.ndim == 2: # 4x4
+                if hdkx.ndim == 2:  # 4x4
                     prod1 = np.einsum('ijk,il,ljk->jk', psi_n.conj(), hdkx, psi_m,
                                 optimize=True)
                     prod2 = np.einsum('ijk,il,ljk->jk', psi_m.conj(), hdky, psi_n,

blg_strain/hamiltonian.py
@@ -127,13 +127,18 @@ def H_2by2(Kx, Ky, xi=1, Delta=0, delta=0, theta=0):
     pi = xi * px + 1j * py
     pidag = xi * px - 1j * py
 
-    # Moulsdale with added xi * v3 * pi (was this an intentional omission?)
+    pi2 = px + 1j * py
+    pidag2 = px - 1j * py
+
+    # Moulsdale with added trigonal warping terms.
+    # Note: involves pi == px + i*py (no xi)
     H = np.array([
-        [-Deltao / 2, -pidag ** 2 / (2 * meff) + xi * v3 * pi + w3],
-        [-pi **2 / (2 * meff) + xi * v3 * pidag + w3s,   Deltao / 2]
+        [-Deltao / 2, -pidag2 ** 2 / (2 * meff) + xi * v3 * pi2 + w3],
+        [-pi2 ** 2 / (2 * meff) + xi * v3 * pidag2 + w3s, Deltao / 2]
     ])
 
-    # Battilomo (with missing xi - Berry curvature equal/opposite in valleys)
+    # Battilomo
+    # Berry curvature equal/opposite in valleys
     # H = np.array([
     #     [Deltao / 2, -1/(2*meff) * (px ** 2 - py ** 2) + xi * v3 * px  + w3 + 1j/meff * px * py + 1j * xi * v3 * py],
     #     [-1/(2*meff) * (px ** 2 - py ** 2) + xi * v3 * px  + w3s - 1j/meff * px * py - 1j * xi * v3 * py,   -Deltao / 2]