fff049bc85755038051b0b8e84deb7447c0993ec
Brian Schaefer <physinet@gmail.com>
2020-08-06 13:52:31 -0400

    Saver class based on HDF5 file format
    Supports classes as attributes of other classes.

--- blg_strain/utils/saver.py
+++ blg_strain/utils/saver.py
@@ -8,24 +8,34 @@ class Saver:
         Returns a Saver class object with parameters loaded from the .npz file
         at location `filename`.
         '''
-        obj = cls()  # inizialize class object
 
-        data = np.load(filename, allow_pickle=True)
-
-        # Set saved variables as attributes to the class object
-        for attr in data.files:
-            setattr(obj, attr, data[attr].item())
+        with h5py.File(filename, 'r') as f:
+            def read_layer(group):
+                obj = cls()  # inizialize class object
+                for k, v in group.items():
+                    if isinstance(v, h5py.Group): # is a group
+                        obj.__dict__[k] = read_layer(v)
+                    else:
+                        obj.__dict__[k] = v[()] # [()] gets values from h5py.Dataset
+                return obj
+            obj = read_layer(f)
 
         return obj
 
 
     def save(self, filename):
         '''
-        Saves data to a compressed .npz file
+        Saves data to a compressed .h5 file
 
         filename: full path of destination file
         '''
-        if filename[-4:] != '.npz':
-            filename += '.npz'
-
-        np.savez_compressed(filename, **self.__dict__)
+        with h5py.File(filename, 'w') as f:
+            # Recursively search dictionary structure of the class
+            def write_layer(group, d):
+                for k, v in d.items():
+                    if hasattr(v, '__dict__'): # classes have __dict__
+                        group2 = group.create_group(k)
+                        write_layer(group2, v.__dict__)
+                    else:
+                        group.create_dataset(k, data=v)
+            write_layer(f, self.__dict__)
