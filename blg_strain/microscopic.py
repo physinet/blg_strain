c5b25052ce9e7ceb7b8550826a44320a45cf2f84
Brian Schaefer <physinet@gmail.com>
2020-09-03 14:34:11 -0400

    Full solution for relaxation-time approximation
    Probably won't use this, but good to have as part of the package.

--- blg_strain/microscopic.py
+++ blg_strain/microscopic.py
@@ -1,8 +1,9 @@
 # Calculation of microscopic quantities from the bands
 
 import numpy as np
-from .utils.const import kB, hbar, m_e
+from .utils.const import kB, hbar, hbar_J, m_e, a, q
 from scipy.interpolate import RectBivariateSpline
+from scipy.integrate import simps
 
 def feq_func(E, EF, T=0):
     '''
@@ -12,7 +13,7 @@ def feq_func(E, EF, T=0):
 
     Arguments:
     - E: Energy (eV) - an array with arbitrary dimensions
-    - EF: Fermi energy (eV)
+    - EF: Fermi level (eV)
     - T: Temperature (K)
     '''
 
@@ -20,6 +21,35 @@ def feq_func(E, EF, T=0):
         f = 1 / (1 + np.exp((E - EF) / (kB * T)))
 
     return f
+
+
+def f_relaxation(kxa, kya, splE, EF, T, Efield, tau, N):
+    '''
+    Solution of the Boltzmann equation under the relaxation time approximation
+    for an equilibrium occupation that follows the Fermi-Dirac distribution.
+    The occupation is distorted by an applied electric field.
+
+    Arguments:
+    - kxa, kya: Nkx, Nky arrays of kxa, kya points
+    - splE: spline for energy for given band (eV)
+    - EF: Fermi level (eV)
+    - T: Temperature (K)
+    - Efield: an electric field in the x direction (V/m)
+    - tau: relaxation time (s)
+    - N: number of points to use in integration
+    '''
+    qxa = np.linspace(kxa.min(), kxa.max(), N)
+
+    Qxa, Kxa, Kya = np.meshgrid(qxa, kxa, kya, indexing='ij')
+    for i in range(Kxa.shape[1]):
+        # make an array ranging from far left of integration window to each Kx
+        Qxa[:, i] = np.linspace(kxa.min()-1e-10, Kxa[0, i], N)
+
+    f0 = feq_func(splE(Qxa, Kya, grid=False), EF, T=T)
+    integrand = np.exp(-hbar_J * (Kxa - Qxa) / (tau * Efield * q * a)) \
+                * f0 / (tau * Efield * q)
+
+    return simps(integrand, Qxa, axis=0)
 
 
 def check_f_boundaries(f, thresh=0.01):
